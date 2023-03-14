a7a6f2f97f47b4443f015a3853e50e508798b0b5
AsadullahBadhan <asadullah.badhan@gmail.com>
2023-03-14 20:24:35 +0600

    login page styling done

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import "./Login.css";
+import { BsFacebook, BsGoogle } from "react-icons/bs";
+import { FcGoogle } from "react-icons/fc";
+import { IconContext } from "react-icons";
+import { Link } from "react-router-dom";
 
 const Login = () => {
 	function handleLogin(e) {
@@ -10,22 +14,57 @@ const Login = () => {
 	return (
 		<div className="login-container">
 			<div className="login-form">
-				<h1>Login</h1>
+				<h2>Login</h2>
 
 				<form onSubmit={handleLogin}>
-					<label htmlFor="email">Email</label>
-					<input type="email" name="email" />
+					<div className="input-container">
+						<input type="email" name="email" required />
+						<label htmlFor="email">
+							<span className="label-name">Email</span>
+						</label>
+					</div>
 
-					<label htmlFor="password">Password</label>
-					<input type="password" name="password" />
+					<div className="input-container">
+						<input type="password" name="password" required />
+						<label htmlFor="password">
+							<span className="label-name">Password</span>
+						</label>
+					</div>
 
 					<button type="submit" className="login">
 						Login
 					</button>
-					<p>forgot password?</p>
+					<Link to="/reset" className="reset">
+						forgot password?
+					</Link>
 				</form>
 
-				<p>Don't have an account? Create an account.</p>
+				<p className="signup-text">
+					Don't have an account? <Link to="/signup">Create an account.</Link>
+				</p>
+			</div>
+			<div className="divider">
+				<hr />
+				<p>Or</p>
+				<hr />
+			</div>
+			<div className="oauth-container">
+				<button>
+					<IconContext.Provider value={{ color: "#3076ff", size: "2rem" }}>
+						<span className="auth-icon">
+							<BsFacebook />
+						</span>
+					</IconContext.Provider>
+					Continue with Facebook
+				</button>
+				<button>
+					<IconContext.Provider value={{ size: "2rem" }}>
+						<span className="auth-icon">
+							<FcGoogle />
+						</span>
+					</IconContext.Provider>
+					Continue with Google
+				</button>
 			</div>
 		</div>
 	);
